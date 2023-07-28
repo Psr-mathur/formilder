85ec5b694f40c0a1779bdcc4ede594d713370b44
Prakash Kumar <Prakashkumarggh@gmail.com>
2023-07-28 09:18:33 +0530

    f f ik w build

--- frontend/src/Base.jsx
+++ frontend/src/Base.jsx
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:4000";
-export const BASE_UI_URL = "http://localhost:5173";
-// export const BASE_URL = "https://formilderserver.onrender.com";
-// export const BASE_UI_URL = "https://formilder.onrender.com";
+// export const BASE_URL = "http://localhost:4000";
+// export const BASE_UI_URL = "http://localhost:5173";
+export const BASE_URL = "https://formilderserver.onrender.com";
+export const BASE_UI_URL = "https://formilder.onrender.com";
